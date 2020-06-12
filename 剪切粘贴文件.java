05a7f9f718369b7b32f28f20e54f96491d528a71
tomato-sweet <2528064503@qq.com>
2020-06-12 15:35:30 +0800

    create flie

---
+++ 剪切粘贴文件.java
@@ -0,0 +1,9 @@
+import java.io.File;
+
+public class 剪切粘贴文件 {
+    public static void main(String[] args) {
+        File file = new File("你好.txt");
+        File dest = new File("out/你好.txt");
+        file.renameTo(dest);
+    }
+}
