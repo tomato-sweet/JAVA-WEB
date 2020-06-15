5d5cbdd0964cb0ba8d406a43c6c3342478351f5b
tomato-sweet <2528064503@qq.com>
2020-06-15 15:33:30 +0800

    顺时针打印矩阵

---
+++ MatrixClockwise.java
@@ -0,0 +1,32 @@
+public class MatrixClockwise {
+        public int[] spiralOrder(int[][] matrix) {
+            if(matrix.length == 0) return new int[0];
+            int l = 0, r = matrix[0].length - 1, t = 0, b = matrix.length - 1, x = 0;
+            int[] res = new int[(r + 1) * (b + 1)];
+            while(true) {
+                for(int i = l; i <= r; i++)
+                    res[x++] = matrix[t][i];
+                if(++t > b)
+                    break;
+
+                //从上到下
+                for (int i = t;i <= b;i++)
+                    res[x++] = matrix[i][r];
+                if (--r < l)
+                    break;
+
+                //从右到左
+                for (int i = r;i >= l;i--)
+                    res[x++] = matrix[b][i];
+                if (--b < t)
+                    break;
+
+                //从下到上
+                for(int i = b;i >= t;i--)
+                    res[x++] = matrix[i][l];
+                if (++l > r)
+                    break;
+            }
+            return res;
+        }
+    }
