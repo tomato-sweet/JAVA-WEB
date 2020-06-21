ec5be45ba0fc301b379290e13959ae31aa7d95f8
tomato-sweet <2528064503@qq.com>
2020-06-21 15:37:40 +0800

    记不清了

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    private int v = 10;
+
+    private int get() {
+        try {
+            return v;   // 10
+        } finally {
+            v = 10000;
+        }
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.get()); // 10
+        System.out.println(main.v);     // 10000
+    }
+}
