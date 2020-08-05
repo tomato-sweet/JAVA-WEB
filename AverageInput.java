561a680fb816b47070a929977749a298919a9155
tomato-sweet <2528064503@qq.com>
2020-08-05 16:44:37 +0800

    笔试题

---
+++ AverageInput.java
@@ -0,0 +1,24 @@
+/*
+如果存在相邻的两个代金券金额相等,设其面额为x，小美可以使用这两张代金券换一张面额为x+1的代金券，
+并将其仍放在原来两张券的位置上，每进行一次这样的操作，小美就可以获得1元可以无限期使用的奖励金。
+小美觉得奖励金可太香了，因此她想获得尽可能多的奖励金，请问她最多可以获得多少奖励金。
+输入描述
+输入第一行仅包含一个正整数n，表示小美拥有的代金券数量。(1<=n<=500)
+输入第二行包含n个正整数，每个整数x表示一张代金券的面额，同时这也是系统排出的代金券顺序。(1<=x<=100)
+*/
+
+
+import java.util.Scanner;
+
+public class AverageInput {
+
+    public static void main(String[] args) {
+        System.out.println("请输入五个整数：");
+        Scanner input = new Scanner(System.in);
+        String Str = new String("");
+        System.out.println("返回值：");
+        System.out.println(Str.toString());
+
+
+    }
+}
