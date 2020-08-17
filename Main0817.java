c026b3ed753f5f4ec61dbaa0e0cfffa765451cac
tomato-sweet <2528064503@qq.com>
2020-08-17 17:25:27 +0800

    笔试题 从标准中读取到一个字符串，字符串可包含0个或多个单词，单词以空格或者tab分隔。 如果遇到"undo"字符串，表示撤销前一个字符串；如果遇到"redo"字符串，表示恢复刚才撤销掉的字符串

---
+++ Main0817.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+/*
+从标准中读取到一个字符串，字符串可包含0个或多个单词，单词以空格或者tab分隔。
+如果遇到"undo"字符串，表示撤销前一个字符串；如果遇到"redo"字符串，表示恢复刚才撤销掉的字符串
+ */
+public class Main0817 {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();//字符串str
+
+}
